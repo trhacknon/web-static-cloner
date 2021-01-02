0c8ddc1f130a87530ceb9c32815df162ce5ca206
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2021-01-02 07:10:14 +0700

    Remake kodingan
    Karena ada kesalahan pada saat resolve link nya

--- main.backup.py
+++ main.backup.py
@@ -6,28 +6,26 @@ from urllib.parse import urlparse, urljoin
 class Cloner:
 	def __init__(self, url, project_dir = None, completed = []) -> None:
 		super().__init__()
+		print(url, "DOWNLOADING...")
 		self.request = requests.Session()
 		self.url = url
 		self.urlparsed = urlparse(url)
 		self.project_dir = os.path.abspath(os.path.realpath(project_dir if project_dir else self.urlparsed.netloc))
 		
 		resp = self.request.get(url)
-		if resp.status_code == 200:
-			self.bs4 = BeautifulSoup(resp.text, 'lxml')
+		self.bs4 = BeautifulSoup(resp.text, 'lxml')
 
-			# self.__resolveLink(['a', 'area', 'base', 'link'],'href', self.__resolveBaseurl())
-			# self.__resolveLink(['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video'],'src', self.__resolveBaseurl())
+		self.__resolveLink(['a', 'area', 'base', 'link'],'href', self.__resolveBaseurl())
+		self.__resolveLink(['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video'],'src', self.__resolveBaseurl())
 
-			self.pages = []
-			self.__completed = completed
-			self.startQueue()
-		else:
-			print(f"{url} 404 Not Found.")
+		self.pages = []
+		self.__completed = completed
+		self.startQueue()
 
 	def __resolveBaseurl(self):
 		baseTag = self.bs4.select_one('head > base')
 
-		path = os.path.dirname(urljoin(self.url,'.'))
+		path = f"{self.urlparsed.scheme if self.urlparsed.scheme != '' else 'http' }://{self.urlparsed.netloc}"
 		if baseTag:
 			href = baseTag.get('href')
 			urlparsed = urlparse(href)
@@ -47,8 +45,24 @@ class Cloner:
 
 		return os.path.abspath(f"{path}{params}{query}{fragment}")
 
+	def __resolveLink(self, elements, mode, baseurl = ''):
+		for el in self.bs4.find_all(elements):
+			link = el.get(mode)
+			if link and link != '#':
+				urlparsed = urlparse(link)
+				if not urlparsed.fragment:
+					if urlparsed.netloc:
+						if urlparsed.netloc == self.urlparsed.netloc:
+							link = urljoin(baseurl, self.__resolveUrlPath(urlparsed).lstrip('/'))
+						else:
+							if not urlparsed.scheme:
+								link = f"{urlparsed.scheme if urlparsed.scheme != '' else 'http' }://{urlparsed.netloc}/{self.__resolveUrlPath(urlparsed)}"
+					else:
+						link = urljoin(baseurl, link)
 
-	def __resolveCssUrl(self, pathfile, linkRaw):
+				el.attrs[mode] = link
+
+	def __resolveCssUrl(self, pathfile):
 		with open(pathfile, 'rb') as file:
 			content = file.read().decode('utf-8')
 			links = re.findall(r'url\((.*?)\)', content)
@@ -56,7 +70,7 @@ class Cloner:
 				link = link.strip('\'').strip('"')
 
 				linkParsed = urlparse(link)
-				link = urljoin(linkRaw, link)
+				link = urljoin(self.__resolveBaseurl(), link)
 				link = urljoin(link, linkParsed.path)
 				linkParsed = urlparse(link)
 				
@@ -81,67 +95,51 @@ class Cloner:
 	def downloadAsset(self, elements, attribute):
 		for el in self.bs4.find_all(elements):
 			link = el.get(attribute)
-			if link and not str(link).startswith('javascript:void') and not str(link).startswith('data:'):
-				linkparse = urlparse(link)
-				
+			linkparse = urlparse(link)
+			if linkparse.path and linkparse.netloc == self.urlparsed.netloc and link not in self.__completed:
+				path = os.path.join(self.project_dir, linkparse.path.lstrip('/'))
+				dirname = os.path.dirname(path)
+				basename = os.path.basename(path)
+				if basename:
+					os.makedirs(dirname, exist_ok=True)
 
-				if not linkparse.netloc or linkparse.netloc == self.urlparsed.netloc:
+					print(link,"DOWNLOADING...")
+					resp = self.request.get(link)
+					with open(path, 'wb') as file:
+						file.write(resp.content if resp.status_code == 200 else str.encode(''))
+						self.__completed.append(link)
+						print(link,"DONE...")
 
-					link = urljoin(self.__resolveBaseurl()+'/', linkparse.path.lstrip('/'))
-
-					linkparse = urlparse(link)
-					
-					if linkparse.netloc == self.urlparsed.netloc and link not in self.__completed:
-						path = os.path.join(self.project_dir, linkparse.path.lstrip('/'))
-						dirname = os.path.dirname(path)
-						basename = os.path.basename(path)
-						
-						if basename and basename != '.':
-							os.makedirs(dirname, exist_ok=True)
-
-							print(link,"DOWNLOADING...")
-
-							resp = self.request.get(link)
-							with open(path, 'wb') as file:
-								file.write(resp.content if resp.status_code == 200 else str.encode(''))
-								print(link,"DONE...")
-
-							if basename.endswith('.css'):
-								self.__resolveCssUrl(path, link)
-
-			self.__completed.append(link)
-
+					if basename.endswith('.css'):
+						self.__resolveCssUrl(path)
 
 	def downloadPage(self):
 		if self.url not in self.__completed:
 			path = os.path.join(self.project_dir, self.urlparsed.path.lstrip('/') if self.urlparsed.path.lstrip('/') else 'index.html')
-			dirname = os.path.dirname(path)
-			basename = os.path.basename(path)
-			if basename:
-				os.makedirs(dirname, exist_ok=True)
 
-				with open(path, 'w') as file:
-					file.write(self.bs4.prettify())
-					self.__completed.append(self.url)
-					
-					print(self.url, "DONE...")
-					
+			self.__resolveLink(['a', 'area', 'base', 'link'],'href')
+			self.__resolveLink(['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video'],'src')
+			
+			with open(path, 'w') as file:
+				file.write(self.bs4.prettify())
+				self.__completed.append(self.url)
+				print(self.url, "DONE...")
+
+			self.__resolveLink(['a', 'area', 'base', 'link'],'href', self.__resolveBaseurl())
+			self.__resolveLink(['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video'],'src', self.__resolveBaseurl())
 
 	def startQueue(self):
-		self.downloadPage()
 		self.downloadAsset(['audio', 'embed', 'iframe', 'img', 'input', 'script', 'source', 'track', 'video'], 'src')
 		self.downloadAsset(['area', 'link'], 'href')
+		self.downloadPage()
 
 		for a in self.bs4.select('a'):
 			url = a.get('href')
-			if url and '#' not in url:
+			if url and url != '#' and url not in self.__completed:
 				urlparsed = urlparse(url)
-				if not urlparsed.netloc or urlparsed.netloc == self.urlparsed.netloc:
-					url = urljoin(self.__resolveBaseurl()+'/', urlparsed.path.lstrip('/'))
-					if url not in self.__completed:
-						Cloner(url, project_dir=self.project_dir, completed=self.__completed)
+				if urlparsed.netloc == self.urlparsed.netloc:
+					Cloner(url, project_dir=self.project_dir, completed=self.__completed)
 
 if __name__ == "__main__":
-	# url = "https://demo.adminkit.io"
-	url = "https://demos.creative-tim.com/argon-dashboard-pro/pages/dashboards/dashboard.html"
+	url = "https://demo.adminkit.io/"
 	cloner = Cloner(url)
